17266e1fbd41a1ed0ec1e6e1c897f4addd148b64
oushi <aisha.mahmoud72007@gmail.com>
2024-07-24 15:45:52 +0200

    create the main Variables

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-document.querySelector(".control-buttons span").onclick = function(){
-  let yourName = prompt("What is your Name?")
-  
-  if(yourName == null || yourName == ""){
+document.querySelector(".control-buttons span").onclick = function () {
+  let yourName = prompt("What is your Name?");
+
+  if (yourName == null || yourName == "") {
     document.querySelector(".name span").innerHTML = "Unknown";
   } else {
     document.querySelector(".name span").innerHTML = yourName;
   }
-  document.querySelector(".control-buttons").remove()
-}
+    document.querySelector(".control-buttons").remove();
+};
+
+let duration = 1000;
+
+let blocksContainer = document.querySelector(".memory-game-blocks");
+
+let blocks = Array.from(blocksContainer.children);
+
+console.log(blocks.length)
+
+let orderRange = [...Array(blocks.length).keys()]
+
+console.log(orderRange)
