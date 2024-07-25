c81556ce019806002e862fc4105ff8e54b1c2bcd
oushi <aisha.mahmoud72007@gmail.com>
2024-07-25 15:18:18 +0200

    Create shuffle Function

--- script.js
+++ script.js
@@ -19,9 +19,49 @@ console.log(blocks.length)
 
 let orderRange = [...Array(blocks.length).keys()]
 
+//console.log(orderRange)
+shuffle(orderRange)
+//console.log(orderRange)
+
 // add order css property to game Blocks
 blocks.forEach((block, index) => {
 
   block.style.order = index
 
 })
+
+// shuffle function
+
+function shuffle(array){
+  // Settings Vars
+  let current = array.length,
+                temp,
+                random;
+
+while(current > 0){
+    // get Random Number
+    random = Math.floor(Math.random() * current);
+
+    // Decrease Length By One
+    current--;
+
+    // [1] Save Current Element in Stash
+    temp = array[current];
+
+    // [2] Current Element = Random Element
+    array[current] = array[random];
+
+    // [3] Random Element = Get Element from Stash
+    array[random] = temp;
+    
+  }
+
+  return array;
+
+}
+
+/*
+  [1] Save Current Element in Stash
+  [2] Current Element = Random Element
+  [3] Random Element = Get Element from Stash
+*/
